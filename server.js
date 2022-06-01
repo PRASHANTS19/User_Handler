46043df9c5612c0377f89115f04e49f118445559
Prashant sahu <95503105+PRASHANTS19@users.noreply.github.com>
2022-06-01 23:39:32 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+//create http server
+const dotenv = require('dotenv')
+const express = require('express');
+const morgan = require('morgan')
+const bodyparser = require('body-parser')
+const path = require('path'); //inbuild in node application
+
+const connectDB = require('./server/database/connection');
+
+const app = express();
+
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT||8080
+
+//log request morgan allow log request
+app.use(morgan('tiny'));
+
+//mongoDB connection
+connectDB();
+
+
+//parse request to body parser module
+app.use(bodyparser.urlencoded({extended:true}))
+
+//set view engine
+app.set("view engine","ejs")
+// app.set("views",path.resolve(__dirname,"views/ejs"))
+
+//load assets
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+
+
+
+//load routers
+app.use('/',require('./server/routes/router'))
+
+
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+//npm start will execute the app
